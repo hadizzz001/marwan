dc48afab0dc6930fc8b93857e552d9331882a9e0
Your Name <you@example.com>
2025-04-05 15:48:03 +0200

    first commit

--- components/Home.js
+++ components/Home.js
@@ -28,13 +28,7 @@ const Home = () => {
           <p className='max-w-lg mt-3 text-xl leading-relaxed text-[#222222]  md:mt-8' dangerouslySetInnerHTML={{ __html: data?.desc }} />
           </div>
           <div className="relative" >
-            {/* Background Image */}
-            <img
-              className="absolute inset-x-0 bottom-0 -mb-48 -translate-x-1/2 left-1/2"
-              src="https://res.cloudinary.com/dea3r5dn7/image/upload/v1743106550/pgsk9zrfdfm2ilfg3pqh.webp"
-              alt="Background"
-            />
-            {/* Fetched Image */}
+           
             {data && data.img && data.img[0] && (
               <img
                 className="relative w-full xl:max-w-lg xl:mx-auto 2xl:origin-bottom 2xl:scale-110"
